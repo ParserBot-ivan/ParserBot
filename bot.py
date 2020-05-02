f77dd0b8cb0b23ffb55284954a28b18439ea75dc
ParserBot-ivan <dvd79149@mail.ru>
2020-05-02 22:58:00 +0500

    Massage

--- bot.py
+++ bot.py
@@ -5,12 +5,12 @@ from bs4 import BeautifulSoup
 import requests as req
 import time
 
-PROXY = 'socks5://userproxy:password@213.136.89.190:12494' 
+#PROXY = 'socks5://userproxy:password@213.136.89.190:12494' 
 TOKEN = "1212450339:AAHeyDwrUnEuwQ1dmugnhs8tKv1POuOIxIY"
 BTC_DOLLAR = "https://www.google.com/search?q=%D0%BA%D1%83%D1%80%D1%81+%D0%B1%D0%B8%D1%82%D0%BA%D0%BE%D0%B8%D0%BD%D0%B0+%D0%B2+%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80%D0%B0%D1%85&oq=rehc+%2Cbnrjbyf+d+&aqs=chrome.1.69i57j0l7.2672j1j7&sourceid=chrome&ie=UTF-8"
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36"}
 bot = telebot.TeleBot(TOKEN)
-apihelper.proxy = {'https': PROXY}
+#apihelper.proxy = {'https': PROXY}
 
 DIFF  = 50 # разница в долларах
 Money = 0.0
